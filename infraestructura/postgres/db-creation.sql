1de1e6de469000e9409a7b83edd203662f7349f5
Eduardo Palacios <manuelpalacios7@gmail.com>
2019-11-09 00:30:40 -0600

    Agregar productos al catálogo de productos de la empresa.

--- infraestructura/postgres/db-creation.sql
+++ infraestructura/postgres/db-creation.sql
@@ -4,3 +4,7 @@ CREATE DATABASE app OWNER postgres ENCODING 'utf-8';
 CREATE TABLE producto (id serial NOT NULL PRIMARY KEY, nombre TEXT, descripcion TEXT);
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE producto TO app;
 GRANT SELECT, USAGE ON SEQUENCE producto_id_seq TO app;
+INSERT INTO producto (nombre, descripcion) VALUES ('T-600', 'Exterminador Serie 600');
+INSERT INTO producto (nombre, descripcion) VALUES ('T-800 101', 'Exterminador Serie 800 Modelo 101');
+INSERT INTO producto (nombre, descripcion) VALUES ('T-800 102', 'Exterminador Serie 800 Modelo 101');
+INSERT INTO producto (nombre, descripcion) VALUES ('T-1000', 'Exterminador Serie 1000 (el más avanzado a su época)');
