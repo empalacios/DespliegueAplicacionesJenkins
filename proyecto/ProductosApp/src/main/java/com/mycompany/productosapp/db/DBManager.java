aedeff082025a27b03cebd501736c74c5a8a5fa0
Eduardo Palacios <manuelpalacios7@gmail.com>
2019-11-10 14:53:00 -0600

    Lanzar una excepción si no se han configurado las variables de entorno de la aplicación.
    Se lanza una excepción con un mensaje indicando que alguna de las
    configuraciones no existe y cómo agregar variables de entorno del
    servidor Payara para crear la configuración necesaria del aplica-
    ción.

--- proyecto/ProductosApp/src/main/java/com/mycompany/productosapp/db/DBManager.java
+++ proyecto/ProductosApp/src/main/java/com/mycompany/productosapp/db/DBManager.java
@@ -17,9 +17,32 @@ public class DBManager {
 
     private DBManager() throws Exception {
         Class.forName(driverClass);
+        /*
+         * La información sobre la configuración se obtiene a partir de variables
+         * entorno. En este caso, ya que la aplicación se ejecutará en el servidor
+         * Payara, se definen dichas variables en como propiedades del servidor de
+         * aplicaciones.
+         */
         connectionString = System.getProperty("db_connection_string");
         user = System.getProperty("db_username");
         password = System.getProperty("db_user_password");
+        if (connectionString == null || connectionString.trim().isEmpty()
+                || user == null || user.trim().isEmpty()
+                || password == null || password.trim().isEmpty()) {
+            throw new Exception("Ha ocurrido un error, no se han creado las variables de entorno de la aplicación.\n"
+                    + "  Para crear las variables de entorno debe:\n"
+                    + "    * Ingresar a consola de administración del servidor de aplicaciones\n"
+                    + "    * Ir a la opción server (Admin Server) del menú\n"
+                    + "    * Ir a la pestaña Properties\n"
+                    + "    * Add Property (nombre de la propiedad y valor de la propiedad)\n"
+                    + "      Las propiedades a configurar son:\n"
+                    + "        * db_connection_string: url de conexión a la base de datos\n"
+                    + "        * db_username: usuario de base de datos con la que se conecta la aplicación\n"
+                    + "        * db_user_password: contraseña del usuario de base de datos\n"
+                    + "  Recargar la aplicación para que obtenga la configuración"
+                    + "    * Ir a la opción Applications del menú\n"
+                    + "    * Dar click en el enlace Reload correspondiente a nuestra aplicación");
+        }
     }
 
     public static DBManager getInstance() throws Exception {
